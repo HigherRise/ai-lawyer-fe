f9fb7a4664987adf93c6914b84fc0fdedb9dc09e
abi-seth <abiheloaf@gmail.com>
2025-02-26 08:30:00 +0200

    chore: codebase initialization

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import { Metadata } from "next";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="scroll-smooth antialiased [font-feature-settings:'ss01']">
+        {children}
+      </body>
+    </html>
+  );
+}
